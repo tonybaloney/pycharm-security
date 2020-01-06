bd28fa9c0d97973d02f60df6610307cf82955461
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-01-06 13:57:42 +1100

    Add a rule resource and a resource loader built into the configuration panel

--- src/main/java/bandit/BanditConfigurationPanel.java
+++ src/main/java/bandit/BanditConfigurationPanel.java
@@ -3,16 +3,29 @@ package bandit;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.project.Project;
+import com.intellij.ui.CollectionListModel;
+import com.intellij.ui.ColoredListCellRenderer;
+import com.intellij.ui.SimpleTextAttributes;
 import com.intellij.ui.TextFieldWithHistoryWithBrowseButton;
 import com.intellij.ui.components.JBList;
+import com.intellij.util.ui.EmptyIcon;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BanditConfigurationPanel implements SearchableConfigurable {
     private final ProjectService state;
@@ -24,10 +37,14 @@ public class BanditConfigurationPanel implements SearchableConfigurable {
     private JRadioButton useSpecificConfigRadioButton;
     private JPanel rootPanel;
     private JBList ruleList;
+    private CollectionListModel<RuleItem> ruleListModel;
+    private List<String> baseRules;
+
 
     public BanditConfigurationPanel(@NotNull Project project) {
         this.state = ProjectService.getInstance(project).getState();
         this.project = project;
+        this.ruleListModel = new CollectionListModel<>();
     }
 
     @NotNull
@@ -42,9 +59,42 @@ public class BanditConfigurationPanel implements SearchableConfigurable {
         return "Bandit";
     }
 
+    public List<RuleItem> loadRulesFile() throws ParserConfigurationException, IOException, SAXException {
+        ArrayList<RuleItem> rules = new ArrayList<>();
+        InputStream in = this.getClass().getClassLoader().getResourceAsStream("rules.xml");
+        DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+        Document doc = docBuilder.parse(in);
+        NodeList doc_rules = doc.getElementsByTagName("rule");
+        for (int i=0; i < doc_rules.getLength(); i++) {
+            NamedNodeMap attrib = doc_rules.item(i).getAttributes();
+            String code = attrib.getNamedItem("code").getNodeValue();
+            String label = attrib.getNamedItem("label").getNodeValue();
+            rules.add(new RuleItem(code, label));
+        }
+        return rules;
+    }
+
     @Nullable
     @Override
     public JComponent createComponent() {
+        this.ruleList.setModel(this.ruleListModel);
+        try {
+            this.ruleListModel.addAll(0, loadRulesFile());
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        this.ruleList.setCellRenderer(new ColoredListCellRenderer() {
+            @Override
+            protected void customizeCellRenderer(@NotNull JList list, Object value, int index, boolean selected, boolean hasFocus) {
+                append(((RuleItem)value).AsString(), SimpleTextAttributes.REGULAR_ATTRIBUTES);
+                setIcon(EmptyIcon.ICON_16);
+            }
+        });
         return rootPanel;
     }
 
@@ -56,5 +106,19 @@ public class BanditConfigurationPanel implements SearchableConfigurable {
     @Override
     public void apply() throws ConfigurationException {
 
+    }
+
+    public class RuleItem {
+        protected String code;
+        protected String label;
+
+        public RuleItem(String code, String label){
+            this.code = code;
+            this.label = label;
+        }
+
+        public String AsString(){
+            return this.code + " : " + this.label;
+        }
     }
 }
