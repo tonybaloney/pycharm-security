6f8af876ffeea590bd2b15f89e201ef16a1ece42
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 10:17:51 +1100

    Update actions

--- .github/actions/action.sh
+++ .github/actions/action.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
 echo "Scanning $1"
-val=1
-echo ::set-output name=val::$val
+/opt/pycharm-community/bin/inspect.sh /code /sources/SecurityInspectionProfile.xml out.log -format plain -v0 2> /dev/null
+echo ::set-output name=result::$(< out.log)
