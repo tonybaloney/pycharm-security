c1b617631ed31b68e6d92924dc89dc138341fbf6
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 16:50:27 +1100

    Dump the directory output

--- .github/actions/action.sh
+++ .github/actions/action.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 cd /github/workspace
 echo "Scanning $1"
-/opt/pycharm-community/bin/inspect.sh $1 /sources/SecurityInspectionProfile.xml out.json -format json -v0 2> errors.log
-cat out.json | python parse.py
+/opt/pycharm-community/bin/inspect.sh $1 /sources/SecurityInspectionProfile.xml out/ -format json -v0 2> errors.log
+ls -aR out/
+cat out/*.json | python parse.py
