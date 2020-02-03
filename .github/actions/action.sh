4bd368533664ec8daf5538c4df0033ed0755a86d
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 11:20:46 +1100

    Add a debugging command

--- .github/actions/action.sh
+++ .github/actions/action.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 cd /github/workspace
 echo "Scanning $1"
+ls -R .
 /opt/pycharm-community/bin/inspect.sh $1 /sources/SecurityInspectionProfile.xml out.log -format plain -v0 2> errors.log
 echo ::set-output name=result::$(< out.log)
 cat errors.log
