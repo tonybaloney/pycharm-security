6204269697a0b4a2a372bcf2196e768895d5f61f
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 16:52:28 +1100

    Use and install Python 3

--- .github/actions/action.sh
+++ .github/actions/action.sh
@@ -3,4 +3,4 @@ cd /github/workspace
 echo "Scanning $1"
 /opt/pycharm-community/bin/inspect.sh $1 /sources/SecurityInspectionProfile.xml out/ -format json -v0 2> errors.log
 ls -aR out/
-cat out/*.json | python parse.py
+cat out/*.json | python3 parse.py
