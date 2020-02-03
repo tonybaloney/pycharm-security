01a029fe41dbd0cffbf7560f637e71f4da3a1ab8
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 11:02:34 +1100

    Update and show some errors

--- .github/actions/action.sh
+++ .github/actions/action.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
-
+cd /code
 echo "Scanning $1"
-/opt/pycharm-community/bin/inspect.sh /code /sources/SecurityInspectionProfile.xml out.log -format plain -v0 2> /dev/null
+/opt/pycharm-community/bin/inspect.sh $1 /sources/SecurityInspectionProfile.xml out.log -format plain -v0 2> errors.log
 echo ::set-output name=result::$(< out.log)
+cat errors.log
