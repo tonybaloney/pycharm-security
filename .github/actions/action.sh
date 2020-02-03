b50ebf21f78a4a6f3c1233746f03bdc541cd9773
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-03 11:15:49 +1100

    Run the scan against the mounted volume

--- .github/actions/action.sh
+++ .github/actions/action.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
-cd /code
+cd /github/workspace
 echo "Scanning $1"
 /opt/pycharm-community/bin/inspect.sh $1 /sources/SecurityInspectionProfile.xml out.log -format plain -v0 2> errors.log
 echo ::set-output name=result::$(< out.log)
