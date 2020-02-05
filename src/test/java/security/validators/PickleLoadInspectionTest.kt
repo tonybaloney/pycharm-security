1b2b4a4696378c283837aa8705c1b28720039f11
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-05 15:51:09 +1100

    Add tests for the PickleLoadInspection and documentation

---
+++ src/test/java/security/validators/PickleLoadInspectionTest.kt
@@ -0,0 +1,86 @@
+package security.validators
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import security.Checks
+import security.SecurityTestTask
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PickleLoadInspectionTest: SecurityTestTask() {
+    @BeforeAll
+    override fun setUp() {
+        super.setUp()
+    }
+
+    @AfterAll
+    override fun tearDown(){
+        super.tearDown()
+    }
+
+    @Test
+    fun `verify description is not empty`(){
+        assertFalse(PickleLoadInspection().staticDescription.isNullOrEmpty())
+    }
+
+    @Test
+    fun `test pickle load`() {
+        var code = """
+            import pickle
+            
+            pickle.load(data)
+        """.trimIndent()
+        testCodeCallExpression(code, 1, Checks.PickleLoadCheck, "test.py", PickleLoadInspection())
+    }
+
+    @Test
+    fun `test pickle loads`() {
+        var code = """
+            import pickle
+            
+            pickle.loads(data)
+        """.trimIndent()
+        testCodeCallExpression(code, 1, Checks.PickleLoadCheck, "test.py", PickleLoadInspection())
+    }
+
+    @Test
+    fun `test cpickle load`() {
+        var code = """
+            import cPickle
+            
+            cPickle.loads(data)
+        """.trimIndent()
+        testCodeCallExpression(code, 1, Checks.PickleLoadCheck, "test.py", PickleLoadInspection())
+    }
+
+    @Test
+    fun `test cpickle loads`() {
+        var code = """
+            import cPickle
+            
+            cPickle.load(data)
+        """.trimIndent()
+        testCodeCallExpression(code, 1, Checks.PickleLoadCheck, "test.py", PickleLoadInspection())
+    }
+
+    @Test
+    fun `test load not pickle`(){
+        var code = """
+            import bicycle
+            
+            bicycle.load(data)
+        """.trimIndent()
+        testCodeCallExpression(code, 0, Checks.PickleLoadCheck, "test.py", PickleLoadInspection())
+    }
+
+    @Test
+    fun `test pickle not load`(){
+        var code = """
+            import pickle
+            
+            pickle.toad(data)
+        """.trimIndent()
+        testCodeCallExpression(code, 0, Checks.PickleLoadCheck, "test.py", PickleLoadInspection())
+    }
+}
