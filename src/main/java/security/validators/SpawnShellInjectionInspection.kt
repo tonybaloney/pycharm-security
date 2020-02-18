8410f05226f0d1dfac995031ff212a9030dae853
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-02-18 17:35:11 +1100

    Spawn shells (#77)
    * Added spawn shell inspections
    
    * Update bandit list

---
+++ src/main/java/security/validators/SpawnShellInjectionInspection.kt
@@ -0,0 +1,36 @@
+package security.validators
+
+import com.intellij.codeInspection.LocalInspectionToolSession
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElementVisitor
+import com.jetbrains.python.inspections.PyInspection
+import com.jetbrains.python.psi.PyCallExpression
+import security.Checks
+import security.helpers.QualifiedNames.getQualifiedName
+import security.helpers.SecurityVisitor
+
+class SpawnShellInjectionInspection : PyInspection() {
+    val check = Checks.SpawnShellInjectionCheck
+
+    override fun getStaticDescription(): String? {
+        return check.getStaticDescription()
+    }
+
+    override fun buildVisitor(holder: ProblemsHolder,
+                              isOnTheFly: Boolean,
+                              session: LocalInspectionToolSession): PsiElementVisitor = Visitor(holder, session)
+
+    private class Visitor(holder: ProblemsHolder, session: LocalInspectionToolSession) : SecurityVisitor(holder, session) {
+        override fun visitPyCallExpression(node: PyCallExpression) {
+            val qualifiedName = getQualifiedName(node) ?: return
+
+            // Check this is one of the possible shell APIs
+            if (listOf(*SpawnShellApis).contains(qualifiedName).not()) return
+
+            if (node.arguments.isNullOrEmpty()) return
+
+            holder.registerProblem(node, Checks.SpawnShellInjectionCheck.getDescription(), ProblemHighlightType.WEAK_WARNING)
+        }
+    }
+}
