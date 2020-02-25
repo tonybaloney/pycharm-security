1a2a9c3cdfd2c31bcba0509fdc4eb2fd68e0be97
Victor Torre <web.ehooo@gmail.com>
2020-02-25 22:38:08 +0100

    Improve false positive (#87)
    * Improve false positive
    
    If an hardcode string is used as arg or kwarg the rule must no be raised
    
    * Improve false positive
    
    Check method kwargs

--- src/main/java/security/validators/DjangoSafeStringInspection.kt
+++ src/main/java/security/validators/DjangoSafeStringInspection.kt
@@ -6,6 +6,8 @@ import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
 import com.jetbrains.python.inspections.PyInspection
 import com.jetbrains.python.psi.PyCallExpression
+import com.jetbrains.python.psi.PyKeywordArgument
+import com.jetbrains.python.psi.PyStringLiteralExpression
 import security.Checks
 import security.helpers.SecurityVisitor
 import security.helpers.calleeMatches
@@ -25,10 +27,19 @@ class DjangoSafeStringInspection : PyInspection() {
 
     private class Visitor(holder: ProblemsHolder, session: LocalInspectionToolSession) : SecurityVisitor(holder, session) {
         val methodNames = arrayOf("SafeString", "mark_safe", "SafeBytes", "SafeUnicode", "SafeText", "do_mark_safe")
+        val methodKwargs = mapOf("mark_safe" to "s", "do_mark_safe" to "value")
         override fun visitPyCallExpression(node: PyCallExpression) {
+            if (node.arguments.isEmpty()) return
             if (skipDocstring(node)) return
             if (!calleeMatches(node, methodNames)) return
             if (!qualifiedNameStartsWith(node, arrayOf("django.utils.safestring", "jinja2.filters"))) return
+            var arg = node.arguments[0]
+            if (arg is PyKeywordArgument){
+                if(!methodKwargs.containsKey(node.callee?.name)) return
+                if(methodKwargs.get(node.callee?.name) != arg.keyword) return
+                arg = arg.valueExpression
+            }
+            if (arg is PyStringLiteralExpression) return
             holder.registerProblem(node, Checks.DjangoSafeStringCheck.getDescription(), ProblemHighlightType.WEAK_WARNING)
         }
     }
