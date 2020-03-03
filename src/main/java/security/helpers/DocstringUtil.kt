3beef75ebbd78661705ba49077c60a1b126a9f12
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-03-03 14:08:36 +1100

    Create 2020.1-EAP branch

--- src/main/java/security/helpers/DocstringUtil.kt
+++ src/main/java/security/helpers/DocstringUtil.kt
@@ -1,13 +1,13 @@
 package security.helpers
 
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.parentOfType
+import com.intellij.psi.util.parentOfTypes
 import com.jetbrains.python.documentation.doctest.PyDocstringFile
 import security.settings.SecuritySettings
 
 fun skipDocstring(node: PsiElement): Boolean {
     if (SecuritySettings.instance.ignoreDocstrings)
-        if (node.parentOfType(PyDocstringFile::class) != null)
+        if (node.parentOfTypes(PyDocstringFile::class) != null)
             return true
     return false
 }
